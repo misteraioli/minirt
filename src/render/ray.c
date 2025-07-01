48a8ed34ee7e935195dbb4d2c49531c66079d3f0
misteraioli <nicolasperez.catalan@gmail.com>
2025-07-01 18:53:39 +0200

    rename var amb

--- src/render/ray.c
+++ src/render/ray.c
@@ -6,7 +6,7 @@
 /*   By: niperez <niperez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 21:13:09 by niperez           #+#    #+#             */
-/*   Updated: 2025/06/30 13:28:26 by niperez          ###   ########.fr       */
+/*   Updated: 2025/07/01 18:52:06 by niperez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,7 @@ static t_vect	colorize(t_light light, t_obj *objs, t_inter inter, t_vect amb)
 void	set_ray_color(t_ray *ray, t_scene *sc)
 {
 	t_inter	inter;
-	t_vect	ambiant;
+	t_vect	amb;
 
 	inter = find_inter(ray, sc->objs);
 	if (inter.dist < 0)
@@ -70,8 +70,8 @@ void	set_ray_color(t_ray *ray, t_scene *sc)
 	{
 		if (prod_dot(ray->dir, inter.norm) > 0)
 			inter.norm = scal_mult(-1, inter.norm);
-		ambiant = scal_mult(sc->amb.ratio / 255,
+		amb = scal_mult(sc->amb.ratio / 255,
 				vect_mult(sc->amb.color, inter.color));
-		ray->color = colorize(sc->light, sc->objs, inter, ambiant);
+		ray->color = colorize(sc->light, sc->objs, inter, amb);
 	}
 }
